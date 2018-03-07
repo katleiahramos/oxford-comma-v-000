32415a4a23e592e5016b37732d4818806ad4ccec
Katleiah <katleiahramos@gmail.com>
2018-03-07 18:35:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
   if array.length <= 2
     return array.join(" and ")
   else
-    last_element = array[-1]
-    arr_without_last = array.pop
+    last_element = array.pop
     binding.pry
-    return arr_without_last.join(",") + ", and" + "#{last_element}"
+    return array.join(",") + ", and" + "#{last_element}"
   end
 end
